0864bddf3a3fec0026bc64328e494a01015c09ae
tobiasuruali <tobias.hoesli@hotmail.com>
2023-05-18 19:52:40 +0200

    added CSVWriter class to save scrape output in csv got rid of total pandas logic for output

--- scraper_ads/scraper_ads/spiders/test_scrape.py
+++ scraper_ads/scraper_ads/spiders/test_scrape.py
@@ -1,8 +1,23 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 import json
 import csv
 import pandas as pd
 from urllib.parse import urlparse, parse_qs
+import time
+
+class CSVWriter:
+    def __init__(self, filename):
+        self.file = open(filename, 'a', newline='')
+        self.writer = csv.DictWriter(self.file, fieldnames=['ad_id','ad_url', 'youtube_url'])
+        self.writer.writeheader()
+    
+    def write_row(self, cr_value, ad_url, youtube_url):
+        self.writer.writerow({'ad_id': cr_value,'ad_url': ad_url, 'youtube_url': youtube_url})
+    
+    def close(self):
+        self.file.close()
+
 
 class AdspiderSpider(scrapy.Spider):
     name = "adspider"
@@ -10,6 +25,8 @@ class AdspiderSpider(scrapy.Spider):
     
     # Read the start_urls from the us_video_rows.fst file
     df = pd.read_parquet('data/data_processed/us_video_ads_sample.parquet')
+    # Initialize the 'youtube_url' column in the DataFrame
+    df['youtube_url'] = ''
     start_urls = df['ad_url'].tolist()
     
     # Set batch size to 8
@@ -31,44 +48,53 @@ class AdspiderSpider(scrapy.Spider):
         "X-Same-Domain": "1",
     }
     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.csv_writer = CSVWriter('results.csv')
+    
     def start_requests(self):
         for i in range(0, len(self.start_urls), self.batch_size):
             batch_urls = self.start_urls[i:i+self.batch_size]
             for url in batch_urls:
-                # Parse the URL to extract the AR* and CR* values 
+                # Parse the URL to extract the AR* and CR* values
                 parsed_url = urlparse(url)
                 path_parts = parsed_url.path.split('/')
                 ar_value = path_parts[2]
                 cr_value = path_parts[4]
-                
+
                 # Generate the body of the POST request
                 body = f'f.req=%7B%221%22%3A%22{ar_value}%22%2C%222%22%3A%22{cr_value}%22%2C%225%22%3A%7B%221%22%3A2%7D%7D'
-                
+
                 yield scrapy.Request(
                     url="https://adstransparency.google.com/anji/_/rpc/LookupService/GetCreativeById?authuser=",
                     callback=self.parse,
                     method='POST',
                     headers=self.headers,
                     body=body,
-                    cb_kwargs=dict(batch_index=i, url_index=self.start_urls.index(url))
+                    cb_kwargs=dict(batch_index=i, url_index=self.start_urls.index(url), cr_value=cr_value, url=url)
                 )
-    
-    def parse(self, response, batch_index, url_index):
+            # Pause for 10 seconds before processing the next batch of start_urls
+            time.sleep(10)
+
+    def parse(self, response, batch_index, url_index, cr_value, url):
         data = json.loads(response.text)
-        
+        # print(data)
+
         try:
             # Extract the YouTube link from the JSON data
             youtube_link = data['1']['5'][0]['2']['4']
-            # Add the youtube_link to the corresponding row in the original DataFrame
-            self.df.loc[url_index, 'youtube_url'] = youtube_link
+            # Write the cr_value and youtube_url to the CSV file
+            self.csv_writer.write_row(cr_value=cr_value, ad_url=url, youtube_url=youtube_link)
+            print(f'Successfully extracted YouTube link for URL index {url_index} : {youtube_link}')
         except:
-            # If there is an error, write 'NoVideo' in the DataFrame
-            self.df.loc[url_index, 'youtube_url'] = 'NoVideo'
-        
-         # Check if all rows in the current batch have been processed
-        if (url_index + 1) % self.batch_size == 0 or url_index == len(self.start_urls) - 1:
-            # Write only the processed rows in the current batch to a CSV file
-            start_index = batch_index * self.batch_size
-            end_index = start_index + self.batch_size
-            self.df.iloc[start_index:end_index].to_csv('processed_urls.csv', mode='a', header=False, index=False)
+            # If there is an error, write 'NoVideo' to the CSV file
+            self.csv_writer.write_row(cr_value=cr_value, ad_url=url, youtube_url='NoVideo')
+            print(f'Failed to extract YouTube link for URL index {url_index}')
     
+    def closed(self, reason):
+        self.csv_writer.close()
+
+# Run the spider using CrawlerProcess
+process = CrawlerProcess()
+process.crawl(AdspiderSpider)
+process.start()
